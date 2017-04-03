e408b0d04a66b927fd1c4f4ddd1113d1791f7640
red <snobbery@outlook.com>
2017-04-03 11:29:54 +0800

    change yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     output:{
         path:path.join(__dirname,'build','public'),
-        filename:'static/[name]+[hash].js',
+        filename:'static/[name]-[hash].js',
         publicPath:'http://d.ev/',
     },
     module:{
