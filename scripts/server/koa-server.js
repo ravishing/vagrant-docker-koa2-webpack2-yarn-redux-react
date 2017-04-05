6b07f1a05362714e8d397964f17ffef2f11aaf3a
red <snobbery@outlook.com>
2017-04-05 18:45:52 +0800

    <chore>(webpack):koa-server.js
    路径引入错误

--- scripts/server/koa-server.js
+++ scripts/server/koa-server.js
@@ -3,7 +3,7 @@ var app=new Koa();
 var webpack = require('webpack');
 var webpackDevMiddleware = require('koa-webpack-dev-middleware');
 var webpackHotMiddleware = require('koa-webpack-hot-middleware');
-var config=require('../webpack.config.js')
+var config=require('../../webpack.config.js')
 var env_mode=process.argv[2].split('=')[1];
 var compiler=webpack(config({
   ENV_MODE:env_mode
