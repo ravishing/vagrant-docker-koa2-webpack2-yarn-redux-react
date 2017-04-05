8ad7d3343b963a0377ca7e2b35cf6ab66a0ff45e
red <snobbery@outlook.com>
2017-04-05 23:11:52 +0800

    <optimize>(uglify):生产环境下多核并行压缩

--- config/webpack/prod.js
+++ config/webpack/prod.js
@@ -1,1 +1,19 @@
-module.exports={};
+const UglifyJsParallelPlugin = require('webpack-uglify-parallel');
+const os = require('os');
+module.exports={
+    output:{
+        filename:'static/[name].[chunkhash:8].js',
+        chunkFilename:'[name].[chunkhash:8].js'
+    },
+    plugins:[
+        new UglifyJsParallelPlugin({
+            workers: os.cpus().length,
+            mangle: true,
+            compressor: {
+                warnings: false,
+                drop_console: true,
+                drop_debugger: true
+            }
+        }),
+    ],
+};
